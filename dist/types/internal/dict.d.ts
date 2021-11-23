8fd2678cc8ac71eac6f8fe839036598a5a861550
xdev1 <xdev1@users.noreply.github.com>
2021-11-23 03:56:28 +0100

    Deletet dist folder

--- dist/types/internal/dict.d.ts
+++
@@ -1,6 +0,0 @@
-export { Dictionary };
-declare class Dictionary {
-    #private;
-    addTranslation(locale: string, category: string, key: string, translation: string | ((params: Record<string, any>) => string)): void;
-    translate(locale: string, category: string, key: string, params?: Record<string, any>): string | null;
-}
